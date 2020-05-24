9b168a52d7e874d60185bc103de09734dd9abea5
William Cole <williamcole@Williams-MacBook-Pro-3.local>
2020-05-24 13:30:57 +0100

    adding in contact button to weddings page

--- src/pages/Weddings.js
+++ src/pages/Weddings.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import Prismic from 'prismic-javascript';
 
 import ScrollToTop from '../components/utils/ScrollTop';
-import profileImage from '../images/profile-image.jpg';
 import SlideShow from '../components/SlideShow';
+import Button from '../components/Shared/Button';
+import { Link } from 'react-router-dom';
 
 class Weddings extends Component {
     constructor(props) {
@@ -31,9 +32,6 @@ class Weddings extends Component {
 			{ pageSize : 25, page : 1 }
 			);
 		}).then(function(response) {
-            console.log('yay', response)
-            console.log('yay', response.results[0].data.main_content[0].text)
-            console.log('yay', response.results[0].data.image_1)
             return response;
         });
         
@@ -42,18 +40,22 @@ class Weddings extends Component {
             slideshowImages: [res.results[0].data.image_1.url, res.results[0].data.image_2.url, res.results[0].data.image_3.url, res.results[0].data.image_4.url, res.results[0].data.image_5.url]
         });
     }
-    
-    // TODO add in contact button that launches weddings modal
 
     render() {
-        console.log('aboutContent', this.state.aboutContent)
         return [
             <ScrollToTop />,
             <main className="content-side-by-side" style={{ marginTop: '60px' }}>
                 <SlideShow
                     slideshowImages={this.state.slideshowImages}
                 />
-                <p>{this.state.aboutContent}</p>
+                <div>
+                    <p style={{ marginBottom: '24px' }}>{this.state.aboutContent}</p>
+                    <Link to={{ pathname: "/contact"}}>
+                            <Button
+                                text="ask a question"
+                            />
+                    </Link> 
+                </div>
             </main>
         ];
     }
